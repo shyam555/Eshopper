23204d66c123e1e986d336b30bf80bc2afd9e7ec
shyam.shinde <shyam.shinde@neosofttech.com>
2017-01-31 20:36:04 +0530

    made changes in category migration.

--- db/migrate/20170116115410_remove_column_from_category.rb
+++ db/migrate/20170116115410_remove_column_from_category.rb
@@ -1,5 +1,6 @@
 class RemoveColumnFromCategory < ActiveRecord::Migration
   def change
+    add_column :categories, :parent, :integer
     remove_column :categories, :parent
     rename_column :categories, :subcategory_id, :category_id
   end
