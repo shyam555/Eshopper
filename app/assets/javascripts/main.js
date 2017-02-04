35f22a066d6ae5e98324918ba0e00bae42b53a67
shyam.shinde <shyam.shinde@neosofttech.com>
2017-02-04 13:14:40 +0530

    made changes  in main_js file

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,52 +1,52 @@
-// /*price range*/
-//
-//  $('#sl2').slider();
-//
-// 	var RGBChange = function() {
-// 	  $('#RGB').css('background', 'rgb('+r.getValue()+','+g.getValue()+','+b.getValue()+')')
-// 	};
-//		
-// /*scroll to top*/
-// function updateQuantity(cart_item_id,cart_item_product_id) {
-//
-// 		var quantity = document.getElementById(cart_item_id).value;
-// 		//var id = $('#cart_item_id').attr('id');
-//     // alert("Input field lost focus."+ quantity);
-//     $.ajax({
-//               type: 'PUT',
-//               url: '/cart_items/'+cart_item_id,
-//               data: {"quantity" :quantity},
-//               dataType : 'script',
-//               success: function() {
-//                 $('#cart_item_id').html('hi')
-//                } 
-//              
-//           });
-// }
-//  $(document).ready(function(){
-//	
-//  	$("#cart_item_id").change(function(){
-//          alert("Input field lost focus."+ quantity);
-//      });
-//  });
-//
-// $(document).ready(function(){
-// 	$(function () {
-// 		$.scrollUp({
-// 	        scrollName: 'scrollUp', // Element ID
-// 	        scrollDistance: 300, // Distance from top/bottom before showing element (px)
-// 	        scrollFrom: 'top', // 'top' or 'bottom'
-// 	        scrollSpeed: 300, // Speed back to top (ms)
-// 	        easingType: 'linear', // Scroll to top easing (see http://easings.net/)
-// 	        animation: 'fade', // Fade, slide, none
-// 	        animationSpeed: 200, // Animation in speed (ms)
-// 	        scrollTrigger: false, // Set a custom triggering element. Can be an HTML string or jQuery object
-// 					//scrollTarget: false, // Set a custom target element for scrolling to the top
-// 	        scrollText: '<i class="fa fa-angle-up"></i>', // Text for element, can contain HTML
-// 	        scrollTitle: false, // Set a custom <a> title if required.
-// 	        scrollImg: false, // Set true to use image
-// 	        activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
-// 	        zIndex: 2147483647 // Z-Index for the overlay
-// 		});
-// 	});
-// });
+/*price range*/
+
+ $('#sl2').slider();
+
+	var RGBChange = function() {
+	  $('#RGB').css('background', 'rgb('+r.getValue()+','+g.getValue()+','+b.getValue()+')')
+	};
+		
+/*scroll to top*/
+function updateQuantity(cart_item_id,cart_item_product_id) {
+
+		var quantity = document.getElementById(cart_item_id).value;
+		//var id = $('#cart_item_id').attr('id');
+    // alert("Input field lost focus."+ quantity);
+    $.ajax({
+              type: 'PUT',
+              url: '/cart_items/'+cart_item_id,
+              data: {"quantity" :quantity},
+              dataType : 'script',
+              success: function() {
+                $('#cart_item_id').html('hi')
+               } 
+             
+          });
+}
+ $(document).ready(function(){
+	
+ 	$("#cart_item_id").change(function(){
+         alert("Input field lost focus."+ quantity);
+     });
+ });
+
+$(document).ready(function(){
+	$(function () {
+		$.scrollUp({
+	        scrollName: 'scrollUp', // Element ID
+	        scrollDistance: 300, // Distance from top/bottom before showing element (px)
+	        scrollFrom: 'top', // 'top' or 'bottom'
+	        scrollSpeed: 300, // Speed back to top (ms)
+	        easingType: 'linear', // Scroll to top easing (see http://easings.net/)
+	        animation: 'fade', // Fade, slide, none
+	        animationSpeed: 200, // Animation in speed (ms)
+	        scrollTrigger: false, // Set a custom triggering element. Can be an HTML string or jQuery object
+					//scrollTarget: false, // Set a custom target element for scrolling to the top
+	        scrollText: '<i class="fa fa-angle-up"></i>', // Text for element, can contain HTML
+	        scrollTitle: false, // Set a custom <a> title if required.
+	        scrollImg: false, // Set true to use image
+	        activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+	        zIndex: 2147483647 // Z-Index for the overlay
+		});
+	});
+});
