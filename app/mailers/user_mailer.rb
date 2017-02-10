a7ab3dbeea0117ec0feeeb90269eec8eddb078c3
shyam.shinde <shyam.shinde@neosofttech.com>
2017-02-10 20:49:12 +0530

    made changes in token

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,6 +4,6 @@ class UserMailer < ApplicationMailer
   def welcome_email(user)
     @user = user
     @url  = 'http://example.com/login'
-    mail(to: @user.email, subject: 'Welcome to Eshopper,'+@user.firstname.capitalize)
+    mail(to: @user.email, subject: 'Welcome to Eshopper')
   end
 end
