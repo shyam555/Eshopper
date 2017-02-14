1e8088e4c080b404ab753c4bd1e151f4d6dd03af
shyam.shinde <shyam.shinde@neosofttech.com>
2017-02-14 15:59:43 +0530

    Order detail page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,7 +4,8 @@ class OrdersController < ApplicationController
   # GET /orders
   # GET /orders.json
   def index
-    @orders = Order.all
+    @orders = current_user.orders
+    #binding.pry
   end
 
   # GET /orders/1
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,5 @@ class Order < ActiveRecord::Base
   belongs_to :user
   belongs_to :address
   has_many :orderitems
+  default_scope -> { order(id: :desc) }
 end
