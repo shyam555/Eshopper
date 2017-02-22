72ed1041bc15bc06d2e93ff25b627ca2b18ea0e7
shyam.shinde <shyam.shinde@neosofttech.com>
2017-02-22 12:49:10 +0530

    removed comment

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -13,18 +13,16 @@ function showImage(image_path){
 function updateQuantity(cart_item_id,cart_item_product_id) {
 
 		var quantity = document.getElementById(cart_item_id).value;
-		//var id = $('#cart_item_id').attr('id');
-    // alert("Input field lost focus."+ quantity);
+	
     $.ajax({
-              type: 'PUT',
-              url: '/cart_items/'+cart_item_id,
-              data: {"quantity" :quantity},
-              dataType : 'script',
-              success: function() {
-                $('#cart_item_id').html('hi')
-               } 
-             
-          });
+      type: 'PUT',
+      url: '/cart_items/'+cart_item_id,
+      data: {"quantity" :quantity},
+      dataType : 'script',
+      success: function() {
+        $('#cart_item_id').html('hi')
+       } 
+  });
 }
 
 function useCoupon(){
