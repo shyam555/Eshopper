6076c8b18999fc12ee2ec18cd27b2c7f4f978fd3
shyam.shinde <shyam.shinde@neosofttech.com>
2017-02-25 14:44:05 +0530

    applied classs collapse-collapse in category

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,12 @@
 class CartItem < ActiveRecord::Base
   belongs_to :user
   belongs_to :product
+
+  # def self.sub_total(cart_items)
+  #   sub_total = 0
+  #   discount = 0
+  #   cart_items.each do |item|
+  #     sub_total += (item.product.price.to_i * item.quantity.to_i) 
+  #   end
+  # end
 end
